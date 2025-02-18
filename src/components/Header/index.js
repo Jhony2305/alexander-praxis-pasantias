b33d1155820b962d10768ed6c224314539db427d
Jhony2305 <87538603+Jhony2305@users.noreply.github.com>
2025-02-17 20:49:35 -0500

    nuevo logos

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -95,7 +95,7 @@ const Header = (props) => {
 											<div className="cate-part">
 												<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-more-vertical"><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle cx="12" cy="19" r="1"></circle></svg><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-more-vertical"><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle cx="12" cy="19" r="1"></circle></svg><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-more-vertical"><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle cx="12" cy="19" r="1"></circle></svg>
 												<ul className="react-category-menu">
-													<li><Link to="#">Categories <i className="arrow_carrot-down"></i></Link> 
+													<li><Link to="#">Institución <i className="arrow_carrot-down"></i></Link> 
 														<ul>
 															<li><Link to="/course">English Book</Link></li>
 															<li><Link to="/course">Math Book</Link></li>
@@ -110,14 +110,7 @@ const Header = (props) => {
 												parentMenu = {parentMenu}
 											/>
 										</ul>                                
-										<div className="searchbar-part"> 
-											<form className="search-form">
-												<input type="text" className="form-input" placeholder="Search Course" />
-												<Link to="/course" className="form-button">
-													<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
-												</Link>
-											</form>
-										</div>
+										
 									</div>
 								</nav>
 							</div>
