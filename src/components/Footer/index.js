0c6cb3a5f7c2d13175f6318e131992a8d68df0cb
Jhony2305 <87538603+Jhony2305@users.noreply.github.com>
2025-03-03 21:34:44 -0500

    pie de pagina praxis
    direeccion maps

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -17,56 +17,53 @@ const Footer = (props) => {
                                     <div className="footer-logo white">
                                         <Link to="/" className="logo-text"><img src={footerLogo ? footerLogo : Logo} alt="Footer Logo" /></Link>
                                     </div>
-                                    <h5 className="footer-subtitle">There are course and event custom <br/>
-                                    post types so you can easily create and<br/> manage course, events.</h5>
+                                    <h5 className="footer-subtitle">Praxis <br/>
+                                   Avalados por <br/>Universidad Hemisferios<br/> Camara Artesanal Pichincha</h5>
                                     <ul className="footer-address">
-                                        <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-phone"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg><a href="tel:+(402)76244183"> +(402) 762 441 83 </a></li>
-                                        <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg><a href="mailto:info@yourdmain.com"> info@echooling.com </a></li>
+                                        <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-phone"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg><a href="tel:+(593)992382355"> +(593) 992382355 </a></li>
+                                        <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-mail"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg><a href="mailto:jhonnyddiaz1995@gmail.com"> info@gmail.com </a></li>
                                     </ul>                               
                                 </div>
                             </div>
                            
                             <div className="col-lg-3 md-mb-30">
                                 <div className="footer-widget footer-widget-2">
-                                    <h3 className="footer-title">About Us</h3>
+                                    <h3 className="footer-title">Nosotros</h3>
                                     <div className="footer-menu">
                                         <ul>
-                                            <li><Link to="/about">About</Link></li>
-                                            <li><Link to="/course">Courses</Link></li>
-                                            <li><Link to="/event">Events</Link></li>
-                                            <li><Link to="#">Career</Link></li>
-                                            <li><Link to="/profile">Become a Teacher</Link></li>
-                                            <li><Link to="/contact">Contact</Link></li>
+                                            <li><Link to="/about">Historia</Link></li>
+                                            <li><Link to="/course">Cursos</Link></li>
+                                            <li><Link to="#">Asesores</Link></li>
+                                            <li><Link to="/instructor">Misión Visión</Link></li>
+                                            <li><Link to="/contact">Contáctanos</Link></li>
                                         </ul>
                                     </div>
                                 </div>
                             </div>
                             <div className="col-lg-3 md-mb-30">
                                 <div className="footer-widget footer-widget-3">
-                                    <h3 className="footer-title">Useful Links</h3>
+                                    <h3 className="footer-title">Enlaces Útiles </h3>
                                     <div className="footer-menu">
                                         <ul>
-                                            <li><Link to="#">Browse Library</Link></li>
-                                            <li><Link to="#">Library</Link></li>
-                                            <li><Link to="#">Partners</Link></li>
-                                            <li><Link to="/blog">News & Blog</Link></li>
-                                            <li><Link to="#">FAQ</Link></li>
-                                            <li><Link to="#">Tutorials</Link></li>
+                                            <li><Link to="https://virtual.praxis.com.ec/" target="_blank">Moodle Praxis</Link></li>
+                                            
                                         </ul>
                                     </div>
                                 </div>
                             </div>
                             <div className="col-lg-3">
                                 <div className="footer-widget footer-widget-4">
-                                    <h3 className="footer-title">Newsletter</h3> 
+                                    <h3 className="footer-title">Sedes</h3> 
                                     <div className="footer3__form">
-                                        <p>Get the latest Echooling news <br />delivered to you inbox</p>
-                                        <form action="#">
-                                            <input type="email" required placeholder="Enter your email" />
-                                            <button className="footer3__form-1">
-                                                <i className="arrow_right"></i>
-                                            </button>
-                                        </form>
+                                    <p>
+                                      Sede Quito <br /> Dirección <br />
+                                       <a href="https://maps.app.goo.gl/e2Gw7wVtZCmHNdXa6" target="_blank">Ver dirección en Google Maps</a>
+                                    </p>
+                                    <p>
+                                      Sede Guayaquill  <br /> Dirección <br />
+                                       <a href="https://maps.app.goo.gl/47SR95iXRjKHF97s6" target="_blank">Ver dirección en Google Maps</a>
+                                    </p>
+                                        
                                     </div>  
                                 </div>
                             </div>
